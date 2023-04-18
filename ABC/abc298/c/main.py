25e049873fb839eea6999dbb9dcce9195b59d5f0
KOSAQ <kosaq.titech@gmail.com>
2023-04-18 13:19:26 +0900

    APG4b

--- ABC/abc298/c/main.py
+++ ABC/abc298/c/main.py
@@ -2,8 +2,6 @@ N = int(input())
 Q = int(input())
 
 for i in range(Q):
-    a = list(map(int, input().split()))
-    if len(a) == 2:
-        print(a[0],a[1])
-    elif len(a) == 3:
-        print(a[0], a[1], a[2])
+    query = list(map(int, input().split()))
+    if len(query) == 3:
+        
