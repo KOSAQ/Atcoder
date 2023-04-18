e6258ac4d98e3f1d6b24ec51bed482b7d5484e48
KOSAQ <kosaq.titech@gmail.com>
2023-04-18 15:57:04 +0900

    APG4b

---
+++ APG4b/ex10.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int A, B;
+    cin >> A >> B;
+    int i=0, j=0;
+
+    while(i < A){
+        cout << "]";
+        i++;
+    }
+    cout << endl;
+    while(j < B){
+        cout << "]";
+        j++;
+    }
+    cout << endl;
+}
