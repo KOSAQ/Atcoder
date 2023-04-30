543a540976306f6d82fa98a5632553f311cae452
KOSAQ <kosaq.titech@gmail.com>
2023-04-30 14:42:21 +0900

    abc300

--- ABC/abc300/c/main.cpp
+++ ABC/abc300/c/main.cpp
@@ -2,19 +2,6 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-    //C[i][j]を中心としたバツのサイズを返す
-int sizex(vector<vector<char> >& vec, int a, int b, int H, int W){
-    int n = 0;
-    
-    while(!(a - n < 0 || a + n >= H || b - n < 0 || b + n >= W)){
-        if(vec[a + n][b + n] == '#' && vec[a + n][b - n] == '#' && vec[a - n][b + n] == '#' && vec[a - n][b - n] == '#'){
-            n++;
-        }
-    }
-    
-    return n - 1;
-}
-
 int main(void){
     int H, W;
     cin >> H >> W;
@@ -30,11 +17,7 @@ int main(void){
 
     rep(i, H){
         rep(j, W){
-            if(C[i][j] == '#'){
-                if(sizex(C, i, j, H, W) != 0){
-                    S[sizex(C, i, j, H, W) - 1]++;
-                }
-            }
+            
         }
     }
 
